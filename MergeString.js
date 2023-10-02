400f821da705ca126f69de1d36ecf2e98d934613
AnkithChowdary <ankithc29@gmail.com>
2023-10-02 20:39:17 +0530

    update

--- MergeString.js
+++ MergeString.js
@@ -1,7 +1,7 @@
 console.log("Hi guys")
 let a="Hi";
 let b="everyone";
-let name="Ankith"
+let name="ANKITH"
 let n=a+b;    //Without spacing
 console.log(n)
 console.log(a,b) // With spacing (method 1)
@@ -9,3 +9,5 @@ let k=a+" "+b;// With spacing (method 2)
 console.log(k)
 let c=`My name is ${name}`;
 console.log(c)
+console.log(name[0]) // acessing elements of string
+console.log(name.toLocaleLowerCase())  // To lower case all the string
