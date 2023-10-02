cb2941f6818a13253a32f64869aaa3cc67f789cb
AnkithChowdary <ankithc29@gmail.com>
2023-10-02 21:02:22 +0530

    update

--- ToRemoveSpacesInSTring.js
+++ ToRemoveSpacesInSTring.js
@@ -4,3 +4,6 @@ console.log(a)
 let b=a.trim();  // removes unwanted spaces
 console.log(b)    // Both are same
 console.log(a.trim())  // Both are same
+console.log(a.lastIndexOf("How"))   // Prints last index of string
+console.log(a.lastIndexOf("you"))
+console.log(a.lastIndexOf("You"))  // Prints -1 because You don't exist in the string
