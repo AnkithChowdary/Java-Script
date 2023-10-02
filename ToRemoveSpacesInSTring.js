c1ae935a031e7247a6e9494c8732d08720aeca9e
AnkithChowdary <ankithc29@gmail.com>
2023-10-02 21:14:40 +0530

    Update

--- ToRemoveSpacesInSTring.js
+++ ToRemoveSpacesInSTring.js
@@ -9,4 +9,4 @@ console.log(a.lastIndexOf("you"))
 console.log(a.lastIndexOf("You"))  // Prints -1 because "You" don't exist in the string.
 console.log(a.includes("are"))  // Prints true
 console.log(a.includes("k"))   // Prints false "K" don't exist in the string.
-console.log(a.slice(4,7))    // from index 4 to 6 excluding last one(7)
+console.log(a.slice(4,7))    // printts from index 4 to 6 excluding last one(7) prints w a
