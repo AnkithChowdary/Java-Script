2ee0c7631a22219725f55970484d4162b694ee60
AnkithChowdary <ankithc29@gmail.com>
2023-10-02 20:02:36 +0530

    update

---
+++ MergeString.js
@@ -0,0 +1,8 @@
+console.log("Hi guys")
+let a="Hi";
+let b="everyone";
+let n=a+b;    //Without spacing
+console.log(n)
+console.log(a,b) // With spacing (method 1)
+let k=a+" "+b;// With spacing (method 2)
+console.log(k)
