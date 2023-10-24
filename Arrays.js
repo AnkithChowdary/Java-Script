97c324ed6a91ffeee09caf058c768398d977f197
AnkithChowdary <ankithc29@gmail.com>
2023-10-24 18:14:15 +0530

    Update

--- Arrays.js
+++ Arrays.js
@@ -3,4 +3,7 @@ console.log(a);
 console.log(a[0]);
 console.log(a.length);             // Gives the array length
 const an=new Array(99,12,13);
-console.log()
+console.log(an);
+// Array inside an array
+const k=[a,'chowdary',1900-25];
+console.log(k);
