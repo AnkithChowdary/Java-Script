f4dcb82c91e8c75ca4cb84937b703f42de558860
Ankith <ankithc29@gmail.com>
2024-02-04 03:29:34 +0530

    Update

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpen = document.querySelectorAll('.show-modal');
+const btnsClosed = document.querySelector('.close-modal');
+
+for (let i = 0; i < btnsOpen.length; i++) {
+  btnsOpen[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+btnsClosed.addEventListener('click', function (){
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+})
