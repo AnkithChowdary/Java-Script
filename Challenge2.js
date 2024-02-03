9bac20a11d781dfecfe97906d322b842c7bedb49
Ankith <ankithc29@gmail.com>
2024-02-03 09:50:28 +0530

    Update

---
+++ Challenge2.js
@@ -0,0 +1,12 @@
+let s1=96;
+let s2=108;
+let s3=89;
+let k1=88;
+let k2=91;
+let k3=110;
+let avg1=(s1+s2+s3)/2;
+let avg2=(k1+k2+k3)/2;
+if(avg1>avg2)
+console.log("team1");
+else
+console.log("team2");
