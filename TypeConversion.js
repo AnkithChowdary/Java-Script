5548366b78d417c5d3a06d1eab1406c0a6049516
Ankith <ankithc29@gmail.com>
2024-02-03 10:02:04 +0530

    Update

---
+++ TypeConversion.js
@@ -0,0 +1,10 @@
+let a='7';    // Minus operator converts into integer
+let b=a-5;
+let r=b+2;   // It is already a integer
+console.log(r);
+
+
+let a1='4';
+let b1=a1+5;    //plus operator concats the string
+console.log(b1);
+
