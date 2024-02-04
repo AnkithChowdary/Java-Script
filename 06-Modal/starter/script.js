d5cf3bacf954555955065d29704b0c2d5ff30b7d
Ankith <ankithc29@gmail.com>
2024-02-04 12:51:56 +0530

    Update

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,6 +4,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnsOpen = document.querySelectorAll('.show-modal');
 const btnsClosed = document.querySelector('.close-modal');
+const close=function(){
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+} 
 
 for (let i = 0; i < btnsOpen.length; i++) {
   btnsOpen[i].addEventListener('click', function () {
@@ -12,6 +16,23 @@ for (let i = 0; i < btnsOpen.length; i++) {
   });
 }
 btnsClosed.addEventListener('click', function (){
-  overlay.classList.add('hidden');
-  modal.classList.add('hidden');
+  // overlay.classList.add('hidden');
+  // modal.classList.add('hidden');
+  close();
+})
+overlay.addEventListener('click',function (){
+  // overlay.classList.add('hidden');
+  // modal.classList.add('hidden');
+  close();
+})
+
+addEventListener('keydown',function (e){
+  if(e.key==='Escape'){
+  if(!modal.classList.contains('hidden')){
+    // overlay.classList.add('hidden');
+    // modal.classList.add('hidden');
+    close();
+  }
+}
+  
 })
