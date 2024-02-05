02fa49dcf68281699b5aa1a0f2d9b4f64bf96d85
Ankith <ankithc29@gmail.com>
2024-02-05 10:46:30 +0530

    Update

---
+++ ObjectDes.js
@@ -0,0 +1,12 @@
+const ankithDetails={
+Name:'Ankith',
+Age:Infinity,
+PhoneNum:8019836715,
+Friends:['Bhomraj','David','Karan']
+
+}
+
+//We can store to single entities
+const {Name:YoName,Age:A,PhoneNum:Num,Friends:bestFriends}=ankithDetails;
+console.log(YoName);
+console.log(bestFriends);
