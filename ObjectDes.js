17c6f623a75795920de3f0135337e2e84227b1e5
Ankith <ankithc29@gmail.com>
2024-02-05 10:58:34 +0530

    Update

--- ObjectDes.js
+++ ObjectDes.js
@@ -10,3 +10,13 @@ Friends:['Bhomraj','David','Karan']
 const {Name:YoName,Age:A,PhoneNum:Num,Friends:bestFriends}=ankithDetails;
 console.log(YoName);
 console.log(bestFriends);
+// Giving default value to array
+const {Friends=[],Name:N}=ankithDetails;
+console.log(Friends,N);
+
+//Mutating Variables
+let a=11;
+let b=4;
+// const obj={a:54,a:4};
+// ({a:4,b:7}=obj);
+console.log(a,b);
